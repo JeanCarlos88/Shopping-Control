0a66ea75ab74b178fee6175a74d8518c8f76043e
Jean Carlos <jeancarlosdias88@gmail.com>
2017-05-25 17:26:16 -0300

    Primeiro Commit

---
+++ js/config.js
@@ -0,0 +1,16 @@
+function setConfig(){
+	var texts = {
+		"title": "App Control"
+	};
+
+	document.title = texts.title;
+	document.getElementById("nav-title").innerHTML = texts.title;
+}
+
+setConfig();
+
+
+
+
+
+
