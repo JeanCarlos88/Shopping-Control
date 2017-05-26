c61c1d054ee12b4f75b63d6536006ac9beedad86
Jean Carlos <jeancarlosdias88@gmail.com>
2017-05-26 15:50:51 -0300

    atualizando 1

--- js/main.js
+++ js/main.js
@@ -20,11 +20,69 @@ function getTotal(list){
 function setList(){
 	var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
 	for (var key in list){
-		table += '<tr><td>'+ list[key].desc +'</td><td>'+ list[key].amount +'</td><td>'+ list[key].value +'</td><td>Edit / delete</td></tr>';
+		table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td><button class="btn btn-defaut" onclick="setUpdate('+key+');">Edit</button> delete</td></tr>';
 	}
 	table += '</tbody>';
 	document.getElementById("list-table").innerHTML = table;
 }
+
+function formatDesc(desc){
+	var str = desc.toLowerCase();
+	str = str.charAt(0).toUpperCase() + str.slice(1);
+	return str;
+}
+
+function formatValue(value){
+	var str = parseFloat(value).toFixed(2) + "";
+	str = str.replace(".",",");
+	str = "$ "+ str;
+	return str;
+}
+
+function addData(){
+	var desc = document.getElementById("desc").value;
+	var amount = document.getElementById("amount").value;
+	var value = document.getElementById("value").value;
+
+	list.unshift({"desc":desc ,"amount":amount ,"value":value });
+	setList(list);
+	getTotal(list);
+}
+
+function setUpdate(id){
+	var obj = list[id];
+	document.getElementById("desc").value = obj.desc;
+	document.getElementById("amount").value = obj.amount;
+	document.getElementById("value").value = obj.value;
+
+	document.getElementById("btn-update").style.display = "inline-block";
+	document.getElementById("btn-add").style.display = "none";
+
+}
+
+function resetForm(){
+	document.getElementById("desc").value = "";
+	document.getElementById("amount").value = "";
+	document.getElementById("value").value = "";
+
+	document.getElementById("btn-update").style.display = "none";
+	document.getElementById("btn-add").style.display = "inline-block";
+
+}
+
+function updateData(){
+	var desc = document.getElementById("desc").value;
+	var amount = document.getElementById("amount").value;
+	var value = document.getElementById("value").value;
+
+	list.unshift({"desc":desc ,"amount":amount ,"value":value });
+	setList(list);
+	
+}
+
+
+
+
 
 setList(list);
 document.write("O total é: R$ " + getTotal(list));
